13187987de9f41272ea683f49d4f48398e562916
Sidney Santiago <stscosta@gmail.com>
2020-03-13 15:59:39 -0300

    Iniciamos o projeto

---
+++ class/Sql.php
@@ -0,0 +1,36 @@
+<?php 
+
+class Sql extends PDO{
+
+	private $conn;
+	public function __construct(){
+		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root","");
+	}
+	
+	public function setParams($statment, $parameters = array()){
+		foreach ($parameters as $key => $value) {
+		 	$statment->bindParam($key, $value);
+		} 
+	}
+	public function setParam($statment, $key, $value){
+		$statment->bindParam($key, $value);	
+	}
+
+	public function query($rowQuery,$params=array()){
+		$stmt = $this->conn -> prepare($rowQuery);
+		$this->setParams($stmt,$params);
+		$stmt->execute();
+		return $stmt;
+	}
+	public function select($rowQuery, $params = array()):array{
+		$stmt = $this->query($rowQuery, $params);
+		return $stmt->fetchall(PDO::FETCH_ASSOC);
+	}
+
+
+}
+
+
+
+
+?>
